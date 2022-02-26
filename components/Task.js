8da11f0011440d149c25853791d53cce06efec47
Massii <massinechiche@gmail.com>
2022-02-26 17:58:35 +0100

    chore: prettier write

--- components/Task.js
+++ components/Task.js
@@ -1,50 +1,50 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function Task({ text }) {
-  return (
-    <View style={styles.item}>
-      <View style={styles.itemLeft}>
-        <View style={styles.square}></View>
-        <Text style={styles.itemText}>{text}</Text>
-      </View>
-      <View style={styles.circular}></View>
-    </View>
-  );
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <View style={styles.square}></View>
+                <Text style={styles.itemText}>{text}</Text>
+            </View>
+            <View style={styles.circular}></View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  item: {
-    backgroundColor: "#282c2e",
-    padding: 15,
-    borderRadius: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 15,
-  },
-  itemLeft: {
-    flexDirection: "row",
-    alignItems: "center",
-    flexWrap: "wrap",
-  },
-  square: {
-    width: 20,
-    height: 20,
-    backgroundColor: "#78abf8",
-    opacity: 0.6,
-    borderRadius: 5,
-    marginRight: 15,
-  },
-  itemText: {
-    maxWidth: "80%",
-    color: "#d7d7df",
-  },
-  circular: {
-    width: 12,
-    height: 12,
-    borderColor: "#5596f6",
-    borderWidth: 2,
-    borderRadius: 5,
-  },
-});
+    item: {
+        backgroundColor: '#282c2e',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 15,
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+    },
+    square: {
+        width: 20,
+        height: 20,
+        backgroundColor: '#78abf8',
+        opacity: 0.6,
+        borderRadius: 5,
+        marginRight: 15,
+    },
+    itemText: {
+        maxWidth: '80%',
+        color: '#d7d7df',
+    },
+    circular: {
+        width: 12,
+        height: 12,
+        borderColor: '#5596f6',
+        borderWidth: 2,
+        borderRadius: 5,
+    },
+})

6294a6c08f148572f684803863434dcb7a31d6c3
Massii <massinechiche@gmail.com>
2022-03-04 16:57:04 +0100

    feat: add text input

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import { TouchableOpacity } from 'react-native'
-import Highlight from './components/Highlight'
+import { useState } from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native'
+// import Highlight from './components/Highlight'
 import Task from './components/Task'
 
 export default function App() {
     return (
         <View style={styles.container}>
             <View style={styles.tasksWrapper}>
-                <Text style={styles.sectionTitle}>Hello John,</Text>
-                <View>
+                <Text style={styles.sectionTitle}>Today's tasks,</Text>
+                {/* <View>
                     <Highlight />
-                </View>
+                </View> */}
                 <View style={styles.items}>
                     <TouchableOpacity>
                         <Task text='Workout at 6:00 PM' />
                     </TouchableOpacity>
                 </View>
             </View>
+
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.writeTaskWrapper}>
+                <TextInput style={styles.input} placeholder={'Write a task'} />
+                <TouchableOpacity>
+                    <View style={styles.addWrapper}>
+                        <Text style={styles.addText}>+</Text>
+                    </View>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
         </View>
     )
 }
@@ -41,30 +49,35 @@ const styles = StyleSheet.create({
     },
     writeTaskWrapper: {
         position: 'absolute',
-        bottom: 60,
+        bottom: 30,
+        paddingHorizontal: 20,
         width: '100%',
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
+        color: '#d7d7df',
     },
     input: {
-        paddingVertical: 15,
-        paddingHorizontal: 15,
-        backgroundColor: '#FFF',
-        borderRadius: 60,
-        borderColor: '#C0C0C0',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        backgroundColor: '#282c2e',
+        borderRadius: 12,
+        borderColor: '#282c2e',
         borderWidth: 1,
         width: 250,
+        color: '#d7d7df',
     },
     addWrapper: {
-        width: 60,
-        height: 60,
-        backgroundColor: '#FFF',
-        borderRadius: 60,
+        width: 50,
+        height: 50,
+        backgroundColor: '#282c2e',
+        borderRadius: 12,
         justifyContent: 'center',
         alignItems: 'center',
-        borderColor: '#C0C0C0',
+        borderColor: '#282c2e',
         borderWidth: 1,
     },
-    addText: {},
+    addText: {
+        color: '#78abf8',
+    },
 })
